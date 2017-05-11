7566ff529c8701ab6a932e8875c5e261eaa846f7
Kalcsú Attila <attila.kalcsu@gmail.com>
2017-05-11 10:05:22 +0200

    Media könyvtár használata

--- birkafarkas/PalyaKep.cs
+++ birkafarkas/PalyaKep.cs
@@ -23,79 +23,85 @@ namespace birkafarkas
 
         public PalyaKep() : base()
         {
-            sheep = Image.FromFile(@"c:\__ati\sheep.png");
-            wolf = Image.FromFile(@"c:\__ati\wolf.png");
+            sheep = Image.FromFile(@".\Media\sheep.png");
+            wolf = Image.FromFile(@".\Media\wolf.png");
+            Redraw();
         }
 
         public void setPalya(Palya palya)
         {
             this.palya = palya;
+            Redraw();
             Invalidate();
+        }
+
+        protected void Redraw()
+        {
+            if (buffer == null)
+                return;
+
+            if (palya == null)
+                return;
+
+            bufferg.Clear(Color.DarkGreen);
+
+            Pen p = new Pen(Color.Blue, 5);
+
+            for (int y = 0; y < 7; y++)
+                for (int x = 0; x < 7; x++)
+                {
+                    Csomopont cs = palya.Csomopontok[x, y];
+                    if (cs != null)
+                    {
+                        int px = ox + A * x;
+                        int py = oy + A * y;
+
+                        int A2 = A;// (int)((A / 2)*0.8);
+
+                        /*
+                        if (cs.Ellista[(int)EIrany.Fel] != null)
+                            bufferg.DrawLine(p, px, py, px, py - A2);
+                        if (cs.Ellista[(int)EIrany.JobbraFel] != null)
+                            bufferg.DrawLine(p, px, py, px + A2, py - A2);*/
+                        if (cs.Ellista[(int)EIrany.Jobbra] != null)
+                            bufferg.DrawLine(p, px, py, px + A2, py);
+                        if (cs.Ellista[(int)EIrany.JobbraLe] != null)
+                            bufferg.DrawLine(p, px, py, px + A2, py + A2);
+                        if (cs.Ellista[(int)EIrany.Le] != null)
+                            bufferg.DrawLine(p, px, py, px, py + A2);
+                        if (cs.Ellista[(int)EIrany.BalraLe] != null)
+                            bufferg.DrawLine(p, px, py, px - A2, py + A2);
+                        /*if (cs.Ellista[(int)EIrany.Balra] != null)
+                            bufferg.DrawLine(p, px, py, px - A2, py);
+                        if (cs.Ellista[(int)EIrany.BalraFel] != null)
+                            bufferg.DrawLine(p, px, py, px - A2, py - A2);*/
+
+                        switch (cs.Tipus)
+                        {
+                            case ECsomopontTipus.Üres:
+                                bufferg.FillEllipse(Brushes.Red, px - 10, py - 10, 20, 20); break;
+                            case ECsomopontTipus.Birka:
+                                bufferg.DrawImage(sheep, px - 64 / 2, py - 64 / 2, 64, 64); break;
+                            case ECsomopontTipus.Farkas:
+                                bufferg.DrawImage(wolf, px - 64 / 2, py - 64 / 2, 64, 64); break;
+                        }
+                    }
+                }
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            bufferg.Clear(Color.DarkGreen);
+            if (buffer != null)
+                e.Graphics.DrawImage(buffer, ClientRectangle.Left, ClientRectangle.Top);
 
-            if (palya != null)
-            {
-                Pen p = new Pen(Color.Blue, 5);
-
-                for (int y = 0; y < 7; y++)
-                    for (int x = 0; x < 7; x++)
-                    {
-                        Csomopont cs = palya.Csomopontok[x, y];
-                        if (cs != null)
-                        {
-                            int px = ox + A * x;
-                            int py = oy + A * y;
-
-                            int A2 = A;// (int)((A / 2)*0.8);
-
-                            /*
-                            if (cs.Ellista[(int)EIrany.Fel] != null)
-                                bufferg.DrawLine(p, px, py, px, py - A2);
-                            if (cs.Ellista[(int)EIrany.JobbraFel] != null)
-                                bufferg.DrawLine(p, px, py, px + A2, py - A2);*/
-                            if (cs.Ellista[(int)EIrany.Jobbra] != null)
-                                bufferg.DrawLine(p, px, py, px + A2, py);
-                            if (cs.Ellista[(int)EIrany.JobbraLe] != null)
-                                bufferg.DrawLine(p, px, py, px + A2, py + A2);
-                            if (cs.Ellista[(int)EIrany.Le] != null)
-                                bufferg.DrawLine(p, px, py, px, py + A2);
-                            if (cs.Ellista[(int)EIrany.BalraLe] != null)
-                                bufferg.DrawLine(p, px, py, px - A2, py + A2);
-                            /*if (cs.Ellista[(int)EIrany.Balra] != null)
-                                bufferg.DrawLine(p, px, py, px - A2, py);
-                            if (cs.Ellista[(int)EIrany.BalraFel] != null)
-                                bufferg.DrawLine(p, px, py, px - A2, py - A2);*/
-
-                            switch (cs.Tipus)
-                            {
-                                case ECsomopontTipus.Üres:
-                                    bufferg.FillEllipse(Brushes.Red, px - 10, py - 10, 20, 20); break;
-                                case ECsomopontTipus.Birka:
-                                    bufferg.DrawImage(sheep, px - 64 / 2, py - 64 / 2, 64, 64); break;
-                                case ECsomopontTipus.Farkas:
-                                    bufferg.DrawImage(wolf, px - 64 / 2, py - 64 / 2, 64, 64); break;
-                            }
-
-
-
-
-                        }
-                    }
-
-                if (selectedX >= 0 && selectedX <= 7
-                    &&
-                    selectedY >= 0 && selectedY <= 7)
-                    bufferg.DrawRectangle(Pens.Yellow,
-                        ox + A * selectedX - A / 2,
-                        oy + A * selectedY - A / 2,
-                        A, 
-                        A);
-            }
-            e.Graphics.DrawImage(buffer, ClientRectangle.Left, ClientRectangle.Top);
+            if (selectedX >= 0 && selectedX <= 7
+                &&
+                selectedY >= 0 && selectedY <= 7)
+                e.Graphics.DrawRectangle(Pens.Yellow,
+                    ox + A * selectedX - A / 2,
+                    oy + A * selectedY - A / 2,
+                    A,
+                    A);
         }
 
         protected override void OnPaintBackground(PaintEventArgs pevent)
@@ -112,6 +118,8 @@ namespace birkafarkas
 
             buffer = new Bitmap(ClientSize.Width, ClientSize.Height);
             bufferg = Graphics.FromImage(buffer);
+
+            Redraw();
 
             Invalidate();
         }
